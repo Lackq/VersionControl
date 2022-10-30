ba149ac4af0908ec06e60f515e5d69d43829d763
lackq <laszlo.simon@stud.uni-corvinus.hu>
2022-10-30 20:29:41 +0100

    gyar_absztrakcio

---
+++ Mikulas/Mikulas/Abstractions/IToyFactory.cs
@@ -0,0 +1,14 @@
+﻿using Mikulas.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mikulas.Abstractions
+{
+    public interface IToyFactory
+    {
+        Toy CreateNew();
+    }
+}
--- Mikulas/Mikulas/Entities/CarFactory.cs
+++ Mikulas/Mikulas/Entities/CarFactory.cs
@@ -7,11 +7,12 @@ using System.Threading.Tasks;
 
 namespace Mikulas.Entities
 {
-    public class BallFactory : IToyFactory
+    class CarFactory : IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
+
     }
 }
