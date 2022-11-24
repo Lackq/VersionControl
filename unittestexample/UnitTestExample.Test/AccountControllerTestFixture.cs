bb7baca068eac236d56c819b69798ddee6b34aa9
Lackq <laszlo.simon@stud.uni-corvinus.hu>
2022-11-24 16:45:39 +0100

    email_validalas_kifejtese

--- unittestexample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ unittestexample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
@@ -12,7 +13,14 @@ namespace UnitTestExample.Test
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
+            //ARRANGE
+            var accountController = new AccountController();
 
+            //ACT
+            var actualResult = accountController.ValidateEmail(email);
+
+            //ASSERT
+            Assert.AreEqual(expectedResult, actualResult);
         }
     }
 }
