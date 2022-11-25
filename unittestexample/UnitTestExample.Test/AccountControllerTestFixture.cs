d9a4a6b7cf553c2fd7210a6c2e384b5aacc72030
Lackq <laszlo.simon@stud.uni-corvinus.hu>
2022-11-25 10:48:37 +0100

    orai_munka

--- unittestexample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ unittestexample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -1,11 +1,14 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using System;
 using System.Activities;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitTestExample.Abstractions;
 using UnitTestExample.Controllers;
+using UnitTestExample.Entities;
 
 namespace UnitTestExample.Test
 {
@@ -31,12 +34,14 @@ TestCase("irf@uni-corvinus.hu", true)]
         }
 
 
-        [Test,
+        [
+            Test,
+            TestCase("Abcdefgh", false),
             TestCase("ABCD1234", false),
-            TestCase("Ab1234", false),
-            TestCase("Abcd1234", false),
-            TestCase("abcd1234", true), 
-            TestCase("abcdABCD", true)]
+            TestCase("abcd1234", false),
+            TestCase("Abcd123", false),
+            TestCase("Abcd1234", true)
+        ]
         public void TestValidatePassword(string password, bool expectedResult)
         {
             //ARRANGE
@@ -97,5 +102,33 @@ TestCase("irf@uni-corvinus.hu", true)]
 
             // Assert
         }
+        [
+    Test,
+    TestCase("irf@uni-corvinus.hu", "Abcd1234")
+]
+        public void TestRegisterApplicationException(string newEmail, string newPassword)
+        {
+            // Arrange
+            var accountServiceMock = new Mock<IAccountManager>(MockBehavior.Strict);
+            accountServiceMock
+                .Setup(m => m.CreateAccount(It.IsAny<Account>()))
+                .Throws<ApplicationException>();
+            var accountController = new AccountController();
+            accountController.AccountManager = accountServiceMock.Object;
+
+            // Act
+            try
+            {
+                var actualResult = accountController.Register(newEmail, newPassword);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOf<ApplicationException>(ex);
+            }
+
+            // Assert
+        }
+
     }
 }
