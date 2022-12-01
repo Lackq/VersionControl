ceccbdb7ba8109a2dc663ecf559f25afede661e6
Lackq <laszlo.simon@stud.uni-corvinus.hu>
2022-12-01 13:22:19 +0100

    classok_2

---
+++ kilencedik_ge7izt/kilencedik_ge7izt/Entities/BirthProbability.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kilencedik_ge7izt.Entities
+{
+    class BirthProbability
+    {
+        public int BirthYear { get; set; }
+        public int NbrOfChildren { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
---
+++ kilencedik_ge7izt/kilencedik_ge7izt/Entities/DeathProbability.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kilencedik_ge7izt.Entities
+{
+    class DeathProbability
+    {
+        public int BirthYear { get; set; }
+        public Gender Gender { get; set; }
+        public bool IsAlive { get; set; }
+
+        public Person()
+        {
+            IsAlive = true;
+        }
+    }
+}
